182c6cc12cb42f91902b62e1a82fd7c9b4475f4a
Carlos Manuel Gallardo Mariscal <GallardoMariscalCM@gmail.com>
2021-09-02 17:36:24 -0700

    Agregue la plantilla de MessageBox

--- Ingenieria_Software/Index.cs
+++ Ingenieria_Software/Index.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ingenieria_Software
 {
-    public partial class Form1 : Form
+    public partial class Index : Form
     {
-        public Form1()
+        public Index()
         {
             InitializeComponent();
         }
--- Ingenieria_Software/Program.cs
+++ Ingenieria_Software/Program.cs
@@ -16,7 +16,7 @@ namespace Ingenieria_Software
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Index());
         }
     }
 }
