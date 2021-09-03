3cf736b5865efc58316ea31126c86e92bad41965
Carlos Manuel Gallardo Mariscal <GallardoMariscalCM@gmail.com>
2021-09-02 17:15:11 -0700

    Agregue el conector con la base de datos

--- Ingenieria_Software/ODB.cs
+++ Ingenieria_Software/ODB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -162,4 +163,4 @@ namespace Ingenieria_Software
 
     #endregion
 }
-}
+
